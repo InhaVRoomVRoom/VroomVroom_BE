08aa2fd764a8329489e5e2cbc77547edd5534f9b
chs <legoholic226@naver.com>
2025-11-07 17:37:32 +0900

    feat: default swagger settings

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import errorMiddleware from './Middleware/error';
+import swaggerUI from 'swagger-ui-express';
+import { RegisterRoutes } from './routes/tsoaRoutes';
+import * as swaggerJson from './config/swagger.json';
 
 dotenv.config();
 
@@ -10,9 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
+RegisterRoutes(app);
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJson));
+
+app.use(errorMiddleware);
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
