df918647c00b410932459e26e533640066e998af
chs <legoholic226@naver.com>
2025-12-20 19:58:22 +0900

    fix: fixed ssh

--- server_run.sh
+++ server_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod 600 ~/.ssh/ssh.pem
+chmod 600 ./.ssh/ssh.pem
 
-ssh -o StrictHostKeyChecking=no -N -L 8188:127.0.0.1:8188 -i /.ssh/ssh.pem ubuntu@runyour.ai &
+ssh -L 8188:127.0.0.1:8188 -i ./.ssh/ssh.pem ubuntu@machine.runyour.ai &
 
 npm run start
