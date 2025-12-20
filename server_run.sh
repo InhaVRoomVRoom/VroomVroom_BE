fa46f80ca261e90dcad54d2636a60b516688e6f9
chs <legoholic226@naver.com>
2025-12-20 20:09:46 +0900

    fix: optimized ssh

--- server_run.sh
+++ server_run.sh
@@ -4,4 +4,12 @@ chmod 600 ./.ssh/ssh.pem
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -L 8188:127.0.0.1:8188 -i ./.ssh/ssh.pem ubuntu@machine.runyour.ai &
 
+SSH_PID=$!
+
+trap "kill $SSH_PID" EXIT
+
+until nc -z localhost 8188; do
+  sleep 0.5
+done
+
 npm run start
